a190ae770fa801c262b908fc48de8aa1e12e5c3f
AlucardVergil <vagelisro@gmail.com>
2024-11-15 16:01:24 +0200

    Restricted X Axis on scrollView content panels

--- Assets/Scripts/ChangeButtonColor.cs
+++ Assets/Scripts/ChangeButtonColor.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeButtonColor : MonoBehaviour
 {
+    private string hexColor = "#FFA806"; // Hex color code (including the '#' symbol)
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,16 @@ public class ChangeButtonColor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void ChangeColor()
+    {
+        if (ColorUtility.TryParseHtmlString(hexColor, out Color color))
+        {
+            // Apply the parsed color to the Image component
+            GetComponent<Image>().color = color;
+        }
         
     }
 }
